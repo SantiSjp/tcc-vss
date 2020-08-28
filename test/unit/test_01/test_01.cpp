ace7ea3925273b10d6c89e4ca6fd8561acb0390d
joao.assis <joao.assis@sita.aero>
2020-08-28 03:42:19 -0300

    Adicionando criacao dos diretorios no /tmp para o step de testes do cicd.

--- test/unit/test_01/test_01.cpp
+++ test/unit/test_01/test_01.cpp
@@ -7,8 +7,8 @@ namespace {
         CommandRobot commandTest("/tmp/vision/capture");
         const id newId = 1;
         commandTest.addRobot(newId, {0,1}, true);
-        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
         ASSERT_EQ(true, commandTest.allyRobots[newId]->ally());
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
     
 
--- test/unit/test_02/test_02.cpp
+++ test/unit/test_02/test_02.cpp
@@ -11,6 +11,7 @@ namespace {
         auto aux = dynamic_cast<PolyM::DataMsg<std::map<int,std::vector<double>>>&>(*cmd).getPayload();
         ASSERT_EQ(1,aux[1][0]);
         ASSERT_EQ(2,aux[1][1]);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
     
 }
